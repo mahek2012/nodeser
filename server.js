c1a6c19d2666d2b370866875af48a0b14b56cf48
mahek2012 <maheksavaliya2012@gmail.com>
2026-04-01 12:53:11 +0530

    node

---
+++ server.js
@@ -0,0 +1,12 @@
+//create a basic server using http method
+const http = require("http");
+
+// create a route file
+//req --> send by user
+//res --> sebd by server
+const server = http.createServer((req, res) => {    
+ res.end("hello world!!");
+});
+
+server.listen(3000);
+//check on browser after run server.js file =>localhost:3000
